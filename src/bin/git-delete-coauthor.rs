c69cc0a9bcec57225d9d71f9b98818a207b14202
Martin Frost <martin@frost.ws>
2020-05-23 16:30:46 +0200

    git-delete-coauthor

---
+++ src/bin/git-delete-coauthor.rs
@@ -0,0 +1,15 @@
+use git_mob::{get_available_coauthors, write_coauthors_file} ;
+use structopt::StructOpt;
+
+#[derive(StructOpt,Debug)]
+struct Opt {
+    /// Initials of the co-author to delete
+    initials: String,
+}
+
+fn main() {
+    let opt = Opt::from_args();
+    let mut authors = get_available_coauthors();
+    authors.remove(&opt.initials);
+    write_coauthors_file(authors);
+}
