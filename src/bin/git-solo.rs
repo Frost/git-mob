b0067e9115eb95b143a4891fea854da65bed8422
Martin Frost <frost@ceri.se>
2021-12-11 00:14:13 +0100

    Use clap for git-solo

--- src/bin/git-solo.rs
+++ src/bin/git-solo.rs
@@ -1,16 +1,16 @@
+use clap::Parser;
 use git_mob::{
     ensure_commit_template_is_set, get_main_author, with_gitmessage_template_path_or_exit,
 };
 use std::fs::File;
-use structopt::StructOpt;
 
-#[derive(StructOpt)]
-#[structopt(name="git-solo")]
-/// Disband the mob and continue working solo
+#[derive(Parser, Debug)]
+#[clap(name = "git-solo", version)]
+/// Disband the mob and continue working solo.
 struct Opt {}
 
 fn main() {
-    let _opt = Opt::from_args();
+    let _opt = Opt::parse();
     let main_author = get_main_author();
     println!("{}", main_author);
 
