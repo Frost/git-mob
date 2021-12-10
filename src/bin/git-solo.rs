c9603845d5038a7e77ae6c0080716327acdac28e
Martin Frost <frost@ceri.se>
2021-12-10 23:48:46 +0100

    Add structopt for git-solo
    This means that we can run `git-solo --help` and get some information on
    what that command is doing. This was missing before.

--- src/bin/git-solo.rs
+++ src/bin/git-solo.rs
@@ -2,8 +2,15 @@ use git_mob::{
     ensure_commit_template_is_set, get_main_author, with_gitmessage_template_path_or_exit,
 };
 use std::fs::File;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(name="git-solo")]
+/// Disband the mob and continue working solo
+struct Opt {}
 
 fn main() {
+    let _opt = Opt::from_args();
     let main_author = get_main_author();
     println!("{}", main_author);
 
