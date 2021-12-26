5c5150fafb94aeb8ca0fc47a8b897317e2623f01
Martin Frost <frost@ceri.se>
2021-12-26 23:35:04 +0100

    Use backwards-compatible bash syntax
    MacOS still uses a very old version of bash, probably for open source
    licencing reasons. Therefore, I cannot use modern substitutions like
    `${foo^^}` and must fall back to using `tr` instead.

--- build-manpages.sh
+++ build-manpages.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+set -e
+
 mkdir -p target/man
 for page in git-{mob,solo,{add,edit,delete}-coauthor}; do
-    mdsh --work_dir . -o - -i docs/$page.md | mandown - ${page^^} 1 | gzip > target/man/$page.1.gz
+    mdsh --work_dir . -o - -i docs/$page.md | \
+        mandown - "$(echo $page | tr '[:lower:]' '[:upper:]')" 1 | \
+        gzip > target/man/$page.1.gz
 done
