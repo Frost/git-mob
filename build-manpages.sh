997b76de7d318c1f86c4ea2e15c9b28d2055c025
Martin Frost <frost@ceri.se>
2023-04-07 23:18:17 +0200

    Prepare release tarball in separate script

--- build-manpages.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-mkdir -p target/man
-for page in git-{mob,solo,{add,edit,delete}-coauthor}.1; do
-    gzip --to-stdout target/release/$page > target/man/$page.gz
-done
