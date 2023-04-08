8055c12bb512828def6d2bccfea5a88240894fdd
Martin Frost <frost@ceri.se>
2023-04-08 23:54:06 +0200

    Include README.md in release

--- prepare-release.sh
+++ prepare-release.sh
@@ -8,4 +8,5 @@ mkdir -p $OUT_DIR/{bin,man}
 for binary in git-{mob,solo,{add,edit,delete}-coauthor}; do
     gzip --to-stdout target/release/$binary.1 > $OUT_DIR/man/$binary.gz
     cp target/release/$binary $OUT_DIR/bin/$binary
+    cp README.md $OUT_DIR/README.md
 done
