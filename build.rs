f4462a36b877a431bfb3798ba5ba6e8b03083a82
Martin Frost <frost@ceri.se>
2023-04-11 16:16:00 +0200

    Generate manpages in a slightly more DRY way

--- build.rs
+++ build.rs
@@ -6,44 +6,26 @@ use std::path::Path;
 #[path = "src/cli.rs"]
 mod cli;
 
+macro_rules! generate_manpage {
+    ($struct:ident) => {
+        let target_dir = env::var("CARGO_TARGET_DIR").unwrap_or("target".to_string());
+        let output_dir = Path::new(&target_dir).join(env::var("PROFILE").unwrap());
+
+        let cmd = cli::$struct::command();
+        let cmd_name = format!("{}.1", cmd.get_name());
+        let man = Man::new(cmd);
+        let mut buffer: Vec<u8> = Default::default();
+        man.render(&mut buffer)?;
+        std::fs::write(output_dir.join(cmd_name), buffer)?;
+    };
+}
+
 fn main() -> std::io::Result<()> {
-    let target_dir = env::var("CARGO_TARGET_DIR").unwrap_or("target".to_string());
-    let output_dir = Path::new(&target_dir).join(env::var("PROFILE").unwrap());
-
-    // git-mob docs
-    let cmd = cli::GitMob::command();
-    let man = Man::new(cmd);
-    let mut buffer: Vec<u8> = Default::default();
-    man.render(&mut buffer)?;
-    std::fs::write(output_dir.join("git-mob.1"), buffer)?;
-
-    // git-solo docs
-    let cmd = cli::GitSolo::command();
-    let man = Man::new(cmd);
-    let mut buffer: Vec<u8> = Default::default();
-    man.render(&mut buffer)?;
-    std::fs::write(output_dir.join("git-solo.1"), buffer)?;
-
-    // git-add-coauthor docs
-    let cmd = cli::GitAddCoauthor::command();
-    let man = Man::new(cmd);
-    let mut buffer: Vec<u8> = Default::default();
-    man.render(&mut buffer)?;
-    std::fs::write(output_dir.join("git-add-coauthor.1"), buffer)?;
-
-    // git-edit-coauthor docs
-    let cmd = cli::GitEditCoauthor::command();
-    let man = Man::new(cmd);
-    let mut buffer: Vec<u8> = Default::default();
-    man.render(&mut buffer)?;
-    std::fs::write(output_dir.join("git-edit-coauthor.1"), buffer)?;
-
-    // git-delete-coauthor docs
-    let cmd = cli::GitDeleteCoauthor::command();
-    let man = Man::new(cmd);
-    let mut buffer: Vec<u8> = Default::default();
-    man.render(&mut buffer)?;
-    std::fs::write(output_dir.join("git-delete-coauthor.1"), buffer)?;
+    generate_manpage!(GitMob);
+    generate_manpage!(GitSolo);
+    generate_manpage!(GitAddCoauthor);
+    generate_manpage!(GitEditCoauthor);
+    generate_manpage!(GitDeleteCoauthor);
 
     Ok(())
 }
